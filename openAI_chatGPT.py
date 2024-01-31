2a4975d262db0b50a096f12a6c56bd5c5312ebfc
tk0987 <109526789+tk0987@users.noreply.github.com>
2024-01-31 19:45:35 +0100

    architecture 310000 times better than grblplotter... without optimization

--- openAI_chatGPT.py
+++ openAI_chatGPT.py
@@ -10,54 +10,69 @@ if gpus:
     except RuntimeError as e:
         print(e)
 # Set the random seeds for reproducibility
-seed = 777
-np.random.seed(seed)
-tf.random.set_seed(seed)
-
-# Generate random 3D TSP data
-def generate_tsp_data(num_cities):
-    np.random.seed(0)
-    return np.random.rand(num_cities, 3)
-
-@tf.function
-def tsp_loss(y_true, y_pred):
-    y_true = tf.cast(tf.sparse.to_dense(y_true), dtype=tf.float32)
-    loss = tf.keras.losses.mean_squared_error(y_true, y_pred)
-    return tf.reduce_mean(loss)
-
-# Create the model - it is a scrap, but working scrap - 18x better than connecting points 'row_wise'
-# kinda crude, and scrap - beware
-num_cities = 1000
-tsp_data = generate_tsp_data(num_cities)
-inputs = tf.keras.layers.Input(shape=(num_cities, 3), batch_size=4)
-lstm1 = tf.keras.layers.SimpleRNN(100, return_sequences=False, return_state=True, input_shape=(num_cities, 3))
-lstm2 = tf.keras.layers.SimpleRNN(100, return_sequences=False, return_state=True, input_shape=(num_cities, 3))
-_1, last_hidden_state1= lstm1(inputs)
-_2, last_hidden_state2= lstm2(inputs)
-# sum1=tf.keras.layers.Add()([last_hidden_state1,last_hidden_state2])
-lstm3 = tf.keras.layers.GRU(100, return_sequences=False, return_state=True, input_shape=(num_cities, 3))
-lstm4 = tf.keras.layers.SimpleRNN(100, return_sequences=False, return_state=True, input_shape=(num_cities, 3))
-_3, last_hidden_state3= lstm3(inputs)
-_4, last_hidden_state4= lstm4(inputs)
-lstm5 = tf.keras.layers.LSTM(100, return_sequences=False, return_state=True, input_shape=(num_cities, 3))
-_51,asdfgt,_=lstm5(inputs)
-sum2=tf.keras.layers.Add()([_3,_4,_1,_2,_51])
-hid_lay=tf.keras.layers.Dense(300, activation="relu")
-_5= hid_lay(sum2)
-outputs = tf.keras.layers.Dense(num_cities, activation='softmax')(_5)
-model = tf.keras.Model(inputs, outputs)
-model.summary()
-# Compile the model
-optimizer = tf.keras.optimizers.Adam()
-model.compile(optimizer=optimizer, loss=tsp_loss)
-
-# Generate training data
-num_samples = 10
-inputs = [generate_tsp_data(num_cities) for i in range (num_samples)]
-outputs_indices = np.zeros((num_samples, num_cities))
-outputs_values = np.zeros((num_samples, num_cities))
-batch_size = 4
 with tf.device('/device:GPU:0'):
+    seed = 777
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+    # Generate random 3D TSP data
+    def generate_tsp_data(num_cities):
+        np.random.seed(0)
+        return np.random.rand(num_cities, 3)
+
+    @tf.function
+    def tsp_loss(y_true, y_pred):
+        y_true = tf.cast(tf.sparse.to_dense(y_true), dtype=tf.float32)
+        loss = tf.keras.losses.mean_squared_error(y_true, y_pred)
+        return tf.reduce_mean(loss)
+
+    # Create the model - it is a scrap, but working scrap - 18x better than connecting points 'row_wise'
+    # kinda crude, and scrap - beware
+    num_cities = 1000
+    def model(n): # looks like a daisy! can you agree? if not, please define 'daisy' in a most general term... this model has even stem (hihi)...
+        inputs=tf.keras.layers.Input(shape=(num_cities, 3), batch_size=4)
+        nd1,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        nd2,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        nd3,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        nd4,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        nd5,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        nd6,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        nd7,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+
+        # sum1=tf.keras.layers.Add()([nd1,nd2,nd3,nd4,nd5,nd6,nd7])
+
+        rd1,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        rd2,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        rd3,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        rd4,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        rd5,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        rd6,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+        rd7,_=tf.keras.layers.SimpleRNN(n,return_sequences=True,return_state=True,go_backwards=True)(inputs)
+
+        sum2=tf.keras.layers.Add()([nd1,nd2,nd3,nd4,nd5,nd6,nd7,rd1,rd2,rd3,rd4,rd5,rd6,rd7])
+
+        middle=tf.keras.layers.Dense(2*n,"elu")(sum2)
+
+        outputs=tf.keras.layers.Dense(num_cities,"relu")(middle)
+
+        return tf.keras.Model(inputs,outputs)
+
+    tsp_data = generate_tsp_data(num_cities)
+
+    model=model(121)
+    model.summary()
+    # Compile the model
+    optimizer = tf.keras.optimizers.AdamW()
+    model.compile(optimizer=optimizer, loss=tsp_loss)
+
+    # Generate training data
+    num_samples = 1
+
+
+    inputs = [generate_tsp_data(num_cities) for i in range (num_samples)]
+    outputs_indices = np.zeros((num_samples, num_cities))
+    outputs_values = np.zeros((num_samples, num_cities))
+    batch_size = 1
     for i in range(num_samples):
         permutation = np.random.permutation(num_cities)
         inputs[i] = tsp_data[permutation]
@@ -101,4 +116,4 @@ with tf.device('/device:GPU:0'):
 
         average_loss = total_loss / num_batches
         print(f"\n\n\nEpoch {epoch + 1}/{num_epochs}, Loss: {average_loss:.6f}\n\n\n")
-        model.save(f"./my_recurrent/model_{epoch}_loss_{average_loss}.h5",overwrite=False)
+        model.save(f"./model_{epoch}_loss_{average_loss}.h5",overwrite=False)
